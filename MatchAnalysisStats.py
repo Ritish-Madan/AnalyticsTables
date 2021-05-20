c6ec635478947240af94029ec53e51c6d1c0a79c
Ritish Madan <ritish_m@outlook.com>
2021-05-20 17:38:41 +0530

    Added Backhand Wins

--- MatchAnalysisStats.py
+++ MatchAnalysisStats.py
@@ -18,6 +18,7 @@ def analysis_stats(match_no, player_no, player_type, player_name):
 
     TotalForwardsWins = len(PrimaryData[ShotCondition & ReverseRowID_Condition & playedByCondition & matchCondition])
 
+    # TODO: Add in the dataframe
     ForeHandWinnerRate = None
     if(TotalForwards == None or TotalForwards <= 0):
         ForeHandWinnerRate = 0
@@ -25,5 +26,29 @@ def analysis_stats(match_no, player_no, player_type, player_name):
         ForeHandWinnerRate = math.ceil((TotalForwardsWins / TotalForwards) * 100)
 
     #Backhand Winner Rate
+
+    # Total Forward and Win Conditions (Common)
+    ShotCondition = PrimaryData['Shot'].str[:2] == 'BH'
+    playedByCondition = PrimaryData['Played_by'] == player_name
+    matchCondition = PrimaryData['Match_No'] == match_no
+
+    # Total Forward Specific Condition
+    ShotnoCondition = PrimaryData['Shot_no'] > 0
+
+    TotalBackhands = len(PrimaryData[ShotCondition & ShotnoCondition & playedByCondition & matchCondition])
+
+    # Win Specific Conditions
+    ReverseRowID_Condition = PrimaryData['Reverse_Row_ID'] == 2
+
+    TotalBackhandWins = len(PrimaryData[ShotCondition & ReverseRowID_Condition & playedByCondition & matchCondition])
+
+    # TODO: Add in the dataframe
+    BackhandWinnerRate = None
+    if (TotalForwards == None or TotalForwards <= 0):
+        BackhandWinnerRate = 0
+    else:
+        BackhandWinnerRate = math.ceil((TotalBackhandWins / TotalBackhands) * 100)
+
+
 
 
